91fa56db6b5815ae95de08a514b11f1b3aaf2809
Piyush Gupta <piyushgupta@microsoft.com>
2024-04-08 16:16:31 +0530

    Reverted AKS Kafka private preview changes

--- kafka/kafka-oauth-producer-consumer/src/main/java/com/microsoft/example/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.microsoft.example;
-
-public class Constants {
-    public static Integer MAX_MESSAGES = 100000;
-    public static Integer NUM_PARTITIONS = 8;
-    public static Short REPLICATION_FACTOR = 3;
-}
